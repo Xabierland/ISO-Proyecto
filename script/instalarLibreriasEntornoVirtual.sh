e1e383b2cf889c143824cdf5dbcd614bf69a5150
xabierland <xabierland@gmail.com>
2022-04-07 18:43:48 +0200

    .

--- script/instalarLibreriasEntornoVirtual.sh
+++ script/instalarLibreriasEntornoVirtual.sh
@@ -3,6 +3,7 @@ function instalarLibreriasEntornoVirtual
     echo "Instalar librerias de Python..."
     sudo /var/www/EHU_analisisdesentimiento/public_html/venv/bin/pip3 install transformers
     sudo /var/www/EHU_analisisdesentimiento/public_html/venv/bin/pip3 install torch
+    #sudo /var/www/EHU_analisisdesentimiento/public_html/venv/bin/pip3 install googletrans #PARA QUE LA APLICACION FUNCIONE CON CUALQUIER LENGUAJE
     echo ""
     read -p "PULSA ENTER PARA CONTINUAR..."
 }
