78b961131cf0ebd295f4ec45531c37fcb2dbb8df
xabierland <xabierland@gmail.com>
2022-05-05 17:51:03 +0200

    .

--- script/testearVirtualHost.sh
+++ script/testearVirtualHost.sh
@@ -1,5 +1,12 @@
 function testearVirtualHost
 {
+    REQUIRED_PKG="net-tools"
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+    echo Comprobando instalacion de for $REQUIRED_PKG: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+        echo "$REQUIRED_PKG no esta instalado. Configurando $REQUIRED_PKG."
+        sudo apt-get --yes install $REQUIRED_PKG 
+    fi
     miip=$(ifconfig | grep "inet" | head -n1 | awk '{print $2}')
     aux=$(xdg-open http://$miip:8888/)
 }
